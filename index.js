1a5dd73f9873373cd5d2afd2e3885bc7489f5673
Jacky Wu <j6qup3bj6@gmail.com>
2016-08-31 23:51:26 +0800

    init

--- index.js
+++ index.js
@@ -20,3 +20,46 @@ app.get('/webhook', function (req, res) {
         res.send('Invalid verify token');
     }
 });
+
+app.post('/webhook', function (req, res) {
+
+  console.log(req.body);
+
+  var messaging_events = req.body.entry[0].messaging;
+  for (var i = 0; i < messaging_events.length; i++) {
+    var event = req.body.entry[0].messaging[i];
+    var sender = event.sender.id;
+    if (event.message && event.message.text) {
+      var text = event.message.text;
+      sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200));
+    }
+  }
+  res.sendStatus(200);
+});
+
+function sendTextMessage(sender, text) {
+
+  var messageData = {
+    text: text
+  };
+
+  (0, _request2.default)({
+    url: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: {
+      access_token: 'EAAChxz0eOssBAPJZCQZAx93U97XBdvAMsVgNnWTstzplVTvfZBK7UpwyW39zwzXhZAkDmSZBwAz1ZBbSnZBTYc9ayDpgT02SXSMNTvtiX8zyxyyNl92J7bGrU8mHuf6iz1ihJcRZAZCgOqKcHS4heAzScvoWNZAhEwyeSI0tgcbKit1wZDZD'
+    },
+    method: 'POST',
+    json: {
+      recipient: {
+        id: sender
+      },
+      message: messageData
+    }
+  }, function (error, response, body) {
+    if (error) {
+      console.log('Error sending message: ', error);
+    } else if (response.body.error) {
+      console.log('Error: ', response.body.error);
+    }
+  });
+}
